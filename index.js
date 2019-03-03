5c0bfda813768a92b265f233189b11d9e174e784
Adrian Prendas Araya <adrian.prendas@rivetlogic.com>
2019-03-03 01:16:13 -0600

    svg animation

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('index.html');
+var express = require('express')
+var app = express()
+var path = require('path')
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(index);
-}).listen(9615);
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/images', express.static(__dirname + '/images'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/animation', express.static(__dirname + '/animation'));
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
+	
+    
+});
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
